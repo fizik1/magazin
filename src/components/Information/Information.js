981b2505db80503ddf03a5e23c4b705df1263fb9
fizik1 <shahobnarpay@gmail.comgit config --global user.email shahobnarpay@gmail.com>
2022-03-19 10:42:51 -0400

    xatolik

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,11 +1,11 @@
-import css from './css/Information.module.css'
+// import css from './css/Information.module.css'
 
 function Information(){
 
     return(
-        <div className={css.xaridqilish}>
+        <div className="xaridqilish">
             <img src="/imgs/section7.png" alt="image" />
-            <div className={css.contents}>
+            <div className="contents">
                 <h4>Создай свой лук на все случаи жизни в “Imperia”</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam sollicitudin a cursus in. Vel sit donec semper magna dignissim. Amet, mi justo at metus vestibulum sollicitudin lacus. Nunc vel egestas fermentum pellentesque sed vitae vel.</p>
                 <button>начать покупки</button>
